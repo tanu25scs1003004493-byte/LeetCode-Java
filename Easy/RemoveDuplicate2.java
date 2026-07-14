e6261312d3aa6e702fd27d46542fefbfa5e3f22d
Tanu Priya <tanu.25scs1003004493@iilm.edu>
2026-07-14 22:33:29 +0530

    Add Remove Duplicates sorted array II

---
+++ Easy/RemoveDuplicate2.java
@@ -0,0 +1,14 @@
+public class RemoveDuplicate2 {
+    public int removeDuplicates(int[] nums) {
+        int k = 0; 
+        for (int num : nums) {
+            if (k < 2 || num != nums[k - 2]) {
+                nums[k++] = num;
+            }
+        }
+        return k;
+    }
+}
+
+    
+
