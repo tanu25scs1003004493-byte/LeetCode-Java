a17a46dfe8bdfbfd9abede46c145f215da845845
Tanu Priya <tanu.25scs1003004493@iilm.edu>
2026-07-29 21:52:46 +0530

    Add Gas Station

---
+++ Medium/134_Gas_Station.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int start = 0, tank = 0, total = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+
+            tank += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return total >= 0 ? start : -1;
+    }
+}
